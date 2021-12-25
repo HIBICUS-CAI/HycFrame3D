7cd0e9861fae70a90664f8f9d734adc1638f0d30
caigenkan <cai_genkan@outlook.com>
2021-12-25 13:29:02 +0900

    prepare part of component container

--- HycFrame3D/02_FrameContent/ComponentContainer.cpp
+++ HycFrame3D/02_FrameContent/ComponentContainer.cpp
@@ -1,9 +1,34 @@
 #include "ComponentContainer.h"
 #include "SceneNode.h"
+#include "ATransformComponent.h"
+#include "AInputComponent.h"
+#include "AInteractComponent.h"
+#include "ATimerComponent.h"
+#include "ACollisionComponent.h"
+#include "AMeshComponent.h"
+#include "ALightComponent.h"
+#include "AAudioComponent.h"
+#include "AParticleComponent.h"
+#include "UTransformComponent.h"
+#include "USpriteComponent.h"
+#include "UAnimateComponent.h"
+#include "UTimerComponent.h"
+#include "UInputComponent.h"
+#include "UInteractComponent.h"
+#include "UButtonComponent.h"
+#include "UAudioComponent.h"
 
 ComponentContainer::ComponentContainer(SceneNode& _sceneNode) :
-    mSceneNodeOwner(_sceneNode),
-    /*mCompVector({}), */mCompMap({})
+    mSceneNodeOwner(_sceneNode), mCompMap({}),
+    mATransformCompVector({}), mAInputCompVector({}),
+    mAInteractCompVector({}), mATimerCompVector({}),
+    mACollisionCompVector({}), mAMeshCompVector({}),
+    mALightCompVector({}), mAAudioCompVector({}), mAParticleCompVector({}),
+    mUTransformCompVector({}), mUSpriteCompVector({}),
+    mUAnimateCompVector({}), mUTimerCompVector({}),
+    mUInputCompVector({}), mUInteractCompVector({}),
+    mUButtonCompVector({}), mUAudioCompVector({}),
+    mInsertFlag({})
 {
 
 }
@@ -15,16 +40,26 @@ ComponentContainer::~ComponentContainer()
 
 Component* ComponentContainer::GetComponent(std::string&& _compName)
 {
-    // TEMP---------------------------
-    return nullptr;
-    // TEMP---------------------------
+    if (mCompMap.find(_compName) != mCompMap.end())
+    {
+        return mCompMap[_compName];
+    }
+    else
+    {
+        return nullptr;
+    }
 }
 
 Component* ComponentContainer::GetComponent(std::string& _compName)
 {
-    // TEMP---------------------------
-    return nullptr;
-    // TEMP---------------------------
+    if (mCompMap.find(_compName) != mCompMap.end())
+    {
+        return mCompMap[_compName];
+    }
+    else
+    {
+        return nullptr;
+    }
 }
 
 void ComponentContainer::AddComponent(COMP_TYPE _type, Component& _comp)
--- HycFrame3D/02_FrameContent/ComponentContainer.h
+++ HycFrame3D/02_FrameContent/ComponentContainer.h
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <vector>
 #include <array>
+#include <queue>
 
 class ComponentContainer
 {
@@ -23,6 +24,27 @@ public:
 
 private:
     const class SceneNode& mSceneNodeOwner;
-    //std::array<std::vector<Component>, (size_t)COMP_TYPE::SIZE> mCompVector;
+
+    std::vector<class ATransformComponent> mATransformCompVector;
+    std::vector<class AInputComponent> mAInputCompVector;
+    std::vector<class AInteractComponent> mAInteractCompVector;
+    std::vector<class ATimerComponent> mATimerCompVector;
+    std::vector<class ACollisionComponent> mACollisionCompVector;
+    std::vector<class AMeshComponent> mAMeshCompVector;
+    std::vector<class ALightComponent> mALightCompVector;
+    std::vector<class AAudioComponent> mAAudioCompVector;
+    std::vector<class AParticleComponent> mAParticleCompVector;
+
+    std::vector<class UTransformComponent> mUTransformCompVector;
+    std::vector<class USpriteComponent> mUSpriteCompVector;
+    std::vector<class UAnimateComponent> mUAnimateCompVector;
+    std::vector<class UTimerComponent> mUTimerCompVector;
+    std::vector<class UInputComponent> mUInputCompVector;
+    std::vector<class UInteractComponent> mUInteractCompVector;
+    std::vector<class UButtonComponent> mUButtonCompVector;
+    std::vector<class UAudioComponent> mUAudioCompVector;
+
+    std::array<std::queue<UINT>, (size_t)COMP_TYPE::SIZE> mInsertFlag;
+
     std::unordered_map<std::string, class Component*> mCompMap;
 };
