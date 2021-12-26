cce63c55f58021ff651a07e09082e3a556cbd329
caigenkan <cai_genkan@outlook.com>
2021-12-26 14:14:22 +0900

    ready for actor components developing

--- HycFrame3D/03_ScriptFuncs/DevUsage.cpp
+++ HycFrame3D/03_ScriptFuncs/DevUsage.cpp
@@ -9,23 +9,10 @@
 void DevUsage(SceneNode* _node)
 {
     ActorObject a0("a0", *_node);
-
-    a0.AddAComponent(COMP_TYPE::A_TRANSFORM);
-    // TODO 先到容器中生成，然后再将初始化完成的obj引用传过去
-
-    _node->AddActorObject(a0);
-
     ATransformComponent atc0("a0-transform", nullptr);
-    ATransformComponent atc1("a0-transform", nullptr);
-    UiObject u0("u0", *_node);
-    u0.AddUComponent(COMP_TYPE::U_ANIMATE);
-    _node->AddUiObject(u0);
-
-    UAnimateComponent uac0("u0-animate", nullptr);
 
     _node->GetComponentContainer()->AddComponent(COMP_TYPE::A_TRANSFORM, atc0);
-    _node->GetComponentContainer()->AddComponent(COMP_TYPE::U_ANIMATE, uac0);
-    _node->GetComponentContainer()->
-        DeleteComponent(COMP_TYPE::A_TRANSFORM, "a0-transform");
-    _node->GetComponentContainer()->AddComponent(COMP_TYPE::A_TRANSFORM, atc1);
+    a0.AddAComponent(COMP_TYPE::A_TRANSFORM);
+
+    _node->AddActorObject(a0);
 }
