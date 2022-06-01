9342f3d926ed1eabbf525a9565d4bccd9eacc634
caigenkan <cai_genkan@outlook.com>
2022-06-01 13:18:55 +0900

    コンポーネントの削除にバグ発生

--- HycFrame3D/03_ScriptFuncs/GM02.cpp
+++ HycFrame3D/03_ScriptFuncs/GM02.cpp
@@ -14,10 +14,18 @@ void RegisterGM02(ObjectFactory* _factory)
         { FUNC_NAME(PlayerUpdate),PlayerUpdate });
     _factory->GetADestoryMapPtr()->insert(
         { FUNC_NAME(PlayerDestory),PlayerDestory });
+    _factory->GetAInitMapPtr()->insert(
+        { FUNC_NAME(BulletInit),BulletInit });
+    _factory->GetAUpdateMapPtr()->insert(
+        { FUNC_NAME(BulletUpdate),BulletUpdate });
+    _factory->GetADestoryMapPtr()->insert(
+        { FUNC_NAME(BulletDestory),BulletDestory });
 }
 
 static ATransformComponent* g_PlayerAtc = nullptr;
 static RSCamera* g_Cam = nullptr;
+
+void CreateBullet(DirectX::XMFLOAT3 _startPos, SceneNode& _scene);
 
 void PlayerInput(AInputComponent* _aic, Timer& _timer)
 {
@@ -35,6 +43,12 @@ void PlayerInput(AInputComponent* _aic, Timer& _timer)
     if (fabsf(g_PlayerAtc->GetProcessingPosition().x) > 80.f)
     {
         g_PlayerAtc->RollBackPositionX();
+    }
+
+    if (InputInterface::IsKeyPushedInSingle(KB_SPACE))
+    {
+        CreateBullet(g_PlayerAtc->GetProcessingPosition(),
+            _aic->GetActorOwner()->GetSceneNode());
     }
 }
 
@@ -65,4 +79,80 @@ void PlayerDestory(AInteractComponent* _aitc)
 {
     g_PlayerAtc = nullptr;
     g_Cam = nullptr;
+}
+
+void CreateBullet(DirectX::XMFLOAT3 _startPos, SceneNode& _scene)
+{
+    static unsigned long long int index = 0;
+    std::string actorName = "bullet-actor" + std::to_string(index++);
+    ActorObject bulletActor(actorName, _scene);
+
+    {
+        ATransformComponent atc(actorName + "-transform", nullptr);
+        _startPos.y += 10.f;
+        atc.ForcePosition(_startPos);
+        atc.ForceScaling({ 0.2f,0.2f,0.2f });
+        bulletActor.AddAComponent(COMP_TYPE::A_TRANSFORM);
+        _scene.GetComponentContainer()->AddComponent(
+            COMP_TYPE::A_TRANSFORM, atc);
+    }
+
+    {
+        AMeshComponent amc(actorName + "-mesh", nullptr);
+        amc.AddMeshInfo("sphere");
+        bulletActor.AddAComponent(COMP_TYPE::A_MESH);
+        _scene.GetComponentContainer()->AddComponent(
+            COMP_TYPE::A_MESH, amc);
+    }
+
+    {
+        ATimerComponent atmc(actorName + "-timer", nullptr);
+        atmc.AddTimer("age");
+        bulletActor.AddAComponent(COMP_TYPE::A_TIMER);
+        _scene.GetComponentContainer()->AddComponent(
+            COMP_TYPE::A_TIMER, atmc);
+    }
+
+    {
+        AInteractComponent aitc(actorName + "-interact", nullptr);
+        aitc.SetInitFunction(BulletInit);
+        aitc.SetUpdateFunction(BulletUpdate);
+        aitc.SetDestoryFunction(BulletDestory);
+        bulletActor.AddAComponent(COMP_TYPE::A_INTERACT);
+        _scene.GetComponentContainer()->AddComponent(
+            COMP_TYPE::A_INTERACT, aitc);
+    }
+
+    _scene.AddActorObject(bulletActor);
+}
+
+bool BulletInit(AInteractComponent* _aitc)
+{
+    _aitc->GetActorOwner()->
+        GetAComponent<ATimerComponent>(COMP_TYPE::A_TIMER)->
+        StartTimer("age");
+    return true;
+}
+
+void BulletUpdate(AInteractComponent* _aitc, Timer& _timer)
+{
+    auto atmc = _aitc->GetActorOwner()->
+        GetAComponent<ATimerComponent>(COMP_TYPE::A_TIMER);
+    if (atmc->GetTimer("age")->IsGreaterThan(1.f))
+    {
+        _aitc->GetActorOwner()->SetObjectStatus(STATUS::NEED_DESTORY);
+        _aitc->GetActorOwner()->GetSceneNode().GetObjectContainer()->
+            DeleteActorObject(const_cast<std::string&>(_aitc->
+                GetActorOwner()->GetObjectName()));
+        return;
+    }
+
+    auto atc = _aitc->GetActorOwner()->
+        GetAComponent<ATransformComponent>(COMP_TYPE::A_TRANSFORM);
+    atc->TranslateZAsix(0.1f * _timer.FloatDeltaTime());
+}
+
+void BulletDestory(AInteractComponent* _aitc)
+{
+
 }
--- HycFrame3D/03_ScriptFuncs/GM02.h
+++ HycFrame3D/03_ScriptFuncs/GM02.h
@@ -9,3 +9,7 @@ void PlayerInput(AInputComponent*, Timer&);
 bool PlayerInit(AInteractComponent*);
 void PlayerUpdate(AInteractComponent*, Timer&);
 void PlayerDestory(AInteractComponent*);
+
+bool BulletInit(AInteractComponent*);
+void BulletUpdate(AInteractComponent*, Timer&);
+void BulletDestory(AInteractComponent*);
