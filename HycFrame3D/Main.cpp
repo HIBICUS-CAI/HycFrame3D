fe29f9749a2d50fe03d2aa0c2c9ed83d99f4f611
caigenkan <cai_genkan@outlook.com>
2022-07-07 14:34:00 +0900

    rename var in main

--- HycFrame3D/Main.cpp
+++ HycFrame3D/Main.cpp
@@ -4,13 +4,13 @@ int WinMain(_In_ HINSTANCE Instance,
             _In_opt_ HINSTANCE PrevInstance,
             _In_ LPSTR CmdLine,
             _In_ int CmdShow) {
-  RootSystem root;
+  RootSystem Root;
 
-  if (root.startUp(Instance, CmdShow)) {
-    root.runGameLoop();
+  if (Root.startUp(Instance, CmdShow)) {
+    Root.runGameLoop();
   }
 
-  root.cleanAndStop();
+  Root.cleanAndStop();
 
   return 0;
 }
