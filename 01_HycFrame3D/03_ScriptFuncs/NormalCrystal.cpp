312f5bf6f52451f026c4c034f4609e900c15f15f
caigenkan <cai_genkan@outlook.com>
2022-07-18 17:06:22 +0900

    update normal crystal

--- 01_HycFrame3D/03_ScriptFuncs/NormalCrystal.cpp
+++ 01_HycFrame3D/03_ScriptFuncs/NormalCrystal.cpp
@@ -1,8 +1,8 @@
 #include "NormalCrystal.h"
 #include "PlayerProcess.h"
+#include <RSUtilityFunctions.h>
 #include <unordered_map>
 
-static const float NCRYSTAL_INTENSITY = 300.f;
 static std::unordered_map<AInteractComponent*, float> g_NCrystalTimerMap = {};
 
 void RegisterNormalCrystal(ObjectFactory* _factory)
@@ -42,10 +42,20 @@ void NCrystalUpdate(AInteractComponent* _aitc, const Timer& _timer)
     }
 
     float lightRatio = found->second / 5.f;
-    if (active) { lightRatio = 1.f; }
+    if (active) { lightRatio = 1.f; } 
+    else {
+      lightRatio = (lightRatio < 0.5f)
+                       ? 0.f
+                       : rs_tool::lerp(0.f, 1.f, (lightRatio - 0.5f) / 0.5f);
+    }
     auto alc = _aitc->getActorOwner()->
         getComponent<ALightComponent>();
-    alc->getLightInfo()->setRSLightIntensity(NCRYSTAL_INTENSITY * lightRatio);
+    dx::XMFLOAT3 Color = {};
+    Color.x = rs_tool::lerp(1.f, 0.f, lightRatio);
+    Color.y = rs_tool::lerp(0.f, 1.f, lightRatio);
+    alc->getLightInfo()->setRSLightAlbedo(Color);
+    alc->getLightInfo()->setRSLightIntensity(
+        rs_tool::lerp(800.f, 300.f, lightRatio));
     alc->getLightInfo()->updateBloomColor();
 
     found->second += _timer.floatDeltaTime() / 1000.f;
